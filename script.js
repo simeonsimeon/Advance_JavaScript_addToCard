c55f9d3429387063f0cb817ec01fb40bc5738f0d
simeonsimeon <lingakumardae@gmail.com>
2022-10-08 13:00:19 -0600

    add

--- script.js
+++ script.js
@@ -2,66 +2,241 @@ let idd;
 var Empty=[];
 let FindId;
 var Count=1;
-// var Qut;
+var Qut;
 var Products = [
     {id:1,Name:"Iphone",Price:50000,Ram:"4GB"},
     {id:2,Name:"Samsung",Price:40000,Ram:"4GB"},
     {id:3,Name:"Oppo",Price:35000,Ram:"4GB"},
     {id:4,Name:"Vivo",Price:30000,Ram:"4GB"}
 ]
- let select=function(){
-     idd=parseInt(prompt("Give Value"));
-     FindId=Products.find(find=>find.id === idd);
-    let  Qut=Count;
-    document.getElementById("show").style.display="block"
-    // console.log(FindId);
-    document.getElementById("show").addEventListener("click",function(){
-        if(Empty.some(obj=> obj.id === idd)){
-            Empty.find(find => {
-               if(find.id === idd){
-                find.Qut++
-               }
-            }); 
-         }else{
-            if(FindId){
-                Empty.push({...FindId,Qut:Count});
-               
-            }else{
-                console.log("Easy");
-            }
-           
-         }
-         let p=  document.createElement("p")
-         p.innerText=JSON.stringify(Empty);
-         document.body.appendChild(p);
-    })
+
+let select=function(){
+idd =0;
+  idd+=parseInt(prompt("Give Value"));
+  console.log(idd);
+ document.getElementById("show").style.display="block"
  
+  
+   document.getElementById("show").addEventListener("click",function(){
+    Callback(idd);
+
+   
+  })
+ }
+ function Callback(idd){
+    Qut=Count;
+  FindId=Products.find(find=>find.id === idd);
+
+  if(Empty.find(obj=> obj.id === idd)){
+    Empty.find(find => {
+       if(find.id === idd){
+        find.Qut++
+        console.log("flag");
+       }
+    }); 
+  }else{
+    if(FindId){
+        Empty.push({...FindId,Qut:Count});
+        console.log("hello");
+       
+    }else{
+        console.log("Easy");
     }
-    console.log(Empty);
-document.getElementById("add").addEventListener("click",function(){
-    select()
+   
+  }
+  let p=  document.createElement("p")
+  p.innerText=JSON.stringify(Empty);
+  document.body.appendChild(p);
+  console.log(Empty);
+ }
+ document.getElementById("add").addEventListener("click",function(){
+  select()
 });
 let Input=document.getElementById("Easy").value;
-console.log(Input);
-  function Remo(){
-    let INput=parseInt(prompt("GIVE"));
-    let FindREmove = Empty.find(find => find.id === INput);
-     if(FindREmove){
+// console.log(Input);
+function Remo(){
+  let INput=parseInt(prompt("GIVE"));
+  let FindREmove = Empty.find(find => find.id === INput);
+   if(FindREmove){
+      if(Empty.some(some => some.id === INput)){
+          Empty.find((find) => {find.Qut--
+          
+            if(find.Qut <= 0){
+              let Wow = Empty.findIndex(item => item.id === INput);
+              console.log(Wow);
+               Empty.splice(Wow,1)
+             }
+          
+          });
+        
+      }else{
         if(Empty.some(some => some.id === INput)){
-            Empty.find(find => find.Qut--);
-            if(find.Qut >= 0){
-             let Wow = Empty.findindex(item => item.id === INput);
-              Empty.splice(Wow,1)
-            }
-        }else{
-          if(Empty.some(some => some.id === INput)){
-            Empty.find(find => find.pop());
-          }
+          Empty.find(find => find.pop());
         }
-     }
-  }
+      }
+   }
+}
 let Remove=document.getElementById("remove");
 Remove.addEventListener("click",function(){
-   Remo()
+ Remo()
 })
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//  let select=function(){
+
+//      idd=parseInt(prompt("Give Value"));
+//      console.log(idd);
+//      FindId=Products.find(find=>find.id === idd);
+//     let  Qut=Count;
+//     document.getElementById("show").style.display="block"
+//     // console.log(FindId);
+//     document.getElementById("show").addEventListener("click",function(){
+//         if(Empty.find(obj=> obj.id === idd)){
+//             Empty.find(find => {
+//                if(find.id === idd){
+//                 find.Qut++
+//                 console.log("flag");
+//                }
+//             }); 
+//          }else{
+//             if(FindId){
+//                 Empty.push({...FindId,Qut:Count});
+//                 console.log("hello");
+               
+//             }else{
+//                 console.log("Easy");
+//             }
+           
+//          }
+//          let p=  document.createElement("p")
+//          p.innerText=JSON.stringify(Empty);
+//          document.body.appendChild(p);
+         
+         
+//     })
+    
+//     }
+  
+
 
